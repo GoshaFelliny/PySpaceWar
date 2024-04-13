92076dd4b7f09a59279d2b436431b44620014759
GoshaFelliny <semen.ss62@gmail.com>
2024-04-13 13:28:21 +0300

    звуки и музыка

--- main.py
+++ main.py
@@ -8,20 +8,34 @@ import random
 
 pg.init()
 pg.font.init()
+pg.mixer.init()
 
+# Все звуки и музыка
+blaster_sound = pg.mixer.Sound('blaster_sound.mp3')
+looser_sound = pg.mixer.Sound('looser_sound.mp3')
+boom_sound = pg.mixer.Sound('boom7.wav')
+
+pg.mixer.music.load('music.mp3')
+pg.mixer.music.play()
+
+# Шрифт
 font = pg.font.SysFont('None', 100)
 
-W, H = 900, 900
 
+# Первоначальные настройки экрана
+W, H = 900, 900
 screen = pg.display.set_mode((W, H))
+
 
 score = 0
 run = True
 
-pg.time.set_timer(pg.USEREVENT, 500)  # Ивент спавна метеоритов
+# Ивент спавна метеоритов
+pg.time.set_timer(pg.USEREVENT, 500)
 
 clock = pg.time.Clock()
 
+# Спрайты
 player = SpaceShip(300, 300)
 helth_bar = HelthBar(W // 2, 50)
 
@@ -32,9 +46,12 @@ bullets = pg.sprite.Group()
 pg.mouse.set_visible(False)
 
 while run:
+    # фон
     screen.blit(pg.image.load('bg.jpg'), (0, 0))
+
     clock.tick(60)
 
+    # основные события
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
@@ -48,31 +65,37 @@ while run:
             bullet = Bullet(player.rect.centerx, player.rect.y)
             all_sprite.add(bullet)
             bullets.add(bullet)
+            blaster_sound.play()
 
+    # попадание бластером
     hits = pg.sprite.groupcollide(bullets, meteors, True, True, pg.sprite.collide_circle)
     for hit in hits:
         if hit:
             score += 1
             exp = Explosion(hit.rect.center)
+            boom_sound.play()
             all_sprite.add(exp)
 
+    # полоска хп
     if pg.sprite.groupcollide([player], meteors, False, True, pg.sprite.collide_circle):
         helth_bar.width -= 30
-
     for m in meteors:
         if 0 < m.rect.x < W and m.rect.y > H:
             helth_bar.width -= 30
             m.kill()
 
+    # gameover
     if helth_bar.width <= 0:
         for sprite in all_sprite:
             sprite.kill()
         game_over_text = font.render(f"Game Over", False, 'white')
         screen.blit(game_over_text, (W // 2 - game_over_text.get_width() // 2, 500))
+        looser_sound.play()
         pg.mouse.set_visible(True)
 
+    # Очки
     score_text = font.render(f"{score}", False, 'white')
-    screen.blit(score_text, (W // 2 - 25, 100))
+    screen.blit(score_text, (W // 2 - score_text.get_width()//2, 100))
 
     all_sprite.draw(screen)
     all_sprite.update()
